1ada5ae308460511a4ef7558228fa798459d1527
TCG <ssibonokuhle@gmail.com>
2021-06-04 11:19:31 +0200

    Changed recipeShow to recipeForm

---
+++ frontend/src/components/RecipeForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+// import { Container, FormGroup } from "reactstrap";
+import { addRecipe } from "../redux/actions/recipe";
+import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
+import { useHistory } from "react-router-dom";
+
+function RecipeForm() {
+  const [formData, setFormData] = useState({
+    recipe: "",
+    ingredients: "",
+  });
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.recipe && formData.ingredients) {
+      dispatch(addRecipe(formData));
+      setFormData({
+        recipe: "",
+        ingredients: "",
+      });
+    }
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit} className="recipe-form">
+        <FormGroup>
+          <Label for="recipe">Recipe</Label>
+          <Input
+            type="text"
+            name="recipe"
+            onChange={handleChange}
+            id="recipe"
+            placeholder="Enter recipe "
+            value={formData.recipe}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="imgredients">Ingredients</Label>
+          <Input
+            type="textarea"
+            name="ingredients"
+            id="ingredients"
+            onChange={handleChange}
+            placeholder="Enter ingredients"
+            value={formData.ingredients}
+          />
+        </FormGroup>
+        <Button color="primary">Add to a recipe</Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default RecipeForm;
