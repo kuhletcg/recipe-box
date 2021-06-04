ad93d9dfa2fbfc45b059513c4ed39803b4e7623a
TCG <ssibonokuhle@gmail.com>
2021-06-04 11:07:26 +0200

    Add file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv/config");
+const cors = require("cors");
+const { recipeRoutes } = require("./routesapi/recipe");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const PORT = 4500;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+recipeRoutes(app);
+app.listen(4500, () => console.log(`Server running on port 4500...`));
