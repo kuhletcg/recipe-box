b56666b834f3b3722c4f7e0f7853c6f84ec8c46f
TCG <ssibonokuhle@gmail.com>
2021-06-04 11:11:21 +0200

    Imported button , modal, modalheader

--- frontend/src/components/EditModal.js
+++ frontend/src/components/EditModal.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { editRecipe } from "../redux/actions/recipe";
+
 function EditModal({ ingredients, id }) {
   const [modal, setModal] = useState(false);
   const [newIngredients, setNewIngredients] = useState(ingredients);
   const dispatch = useDispatch();
   const toggle = () => setModal(!modal);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(editRecipe({ newIngredients, id }));
     setModal(!modal);
   };
+
   return (
     <div>
-      <Button color="danger" onClick={toggle}>
+      <Button color="primary" onClick={toggle}>
         <h3>Edit</h3>
       </Button>
       <Modal isOpen={modal} toggle={toggle}>
@@ -33,7 +36,7 @@ function EditModal({ ingredients, id }) {
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
-            Delete{" "}
+            Remove
           </Button>
         </ModalFooter>
       </Modal>
